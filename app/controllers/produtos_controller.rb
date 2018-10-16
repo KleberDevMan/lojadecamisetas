ac2b6396d6edf52850ce7975db649ce332feb32e
KleberDevMen <klebersubcontas@gmail.com>
2018-10-16 09:33:21 -0300

    Curso finalizado.

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,43 +1,70 @@
 class ProdutosController < ApplicationController
 
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
+    # carrega index
     def index
         @produtos_por_nome = Produto.order(:nome).limit 5
         @produtos_por_preco = Produto.order(:preco).limit 2
     end
 
+    # carrega para cadastro
     def new 
         @produto = Produto.new
-        @departamentos = Departamento.all
+        renderiza :new
     end
 
+    # salva
     def create
-        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
-        @produto = Produto.new valores
+        @produto = Produto.new produto_params
         if @produto.save
             flash[:notice] = "Produto salvo com sucesso"
             redirect_to root_url    
         else
-            render :new
+            renderiza :new
         end
     end
 
+    # carrega para edicao
     def edit
-        id = params[:id]
-        @produto = Produto.find(id)
-        @departamentos = Departamento.all
-        render :new
+        renderiza :edit
     end
 
+    # atualiza
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso"
+            redirect_to root_url
+        else
+            renderiza :edit
+        end
+    end
+
+    # delete
     def destroy
-        id = params[:id]
-        Produto.destroy id
+        @produto.destroy
         redirect_to root_url
     end
 
+    # carrega index com filtro
     def busca
         @nome_a_buscar = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
     end
 
+    private
+
+    def renderiza(view)
+        @departamentos = Departamento.all
+        render view
+    end
+    # pega o produto passado na URI
+    def set_produto
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+    def produto_params
+        params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,11 @@ Rails.application.routes.draw do
   # get "/produtos/new" => "produtos#new"
   # post "/produtos" => "produtos#create"
   # delete "/produtos/:id" => "produtos#destroy", as: :produto
-  
-
-
-
-
-
-
+ 
   root "produtos#index"
   
-  resources :produtos, only: [:new, :create, :destroy, :edit]
+  resources :produtos, only: [:new, :create, :destroy, :edit, :update]
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
-  #get "/produtos/:id" => "produtos#edit", as: :editar_produto
-  
+
   resources :departamentos
 end
